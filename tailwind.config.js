a4145170f1602c4c487ee95a361d122e2bcf066f
Daman Arora <damans227@gmail.com>
2022-09-12 16:23:44 -0400

    complete untill chapter-5

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+
+const { join } = require('path');
+
+module.exports = {
+  content: [
+    join('app', 'components', '**', '*.hbs'),
+    join('app', 'templates', '**', '*.hbs'),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
